ee15a430352f658da8a163c540c63d4b83613721
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-22 15:28:30 -0700

    added the router.js as a require into server.js

--- lab-oscar/lib/server.js
+++ lab-oscar/lib/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
 const http = require('http');
+const router = require('./router.js');
 
-const server = module.exports = http.createServer();
+const server = module.exports = http.createServer(router.route);
